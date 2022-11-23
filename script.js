4f87600664b1be0554412d6485286053a12cd6d1
vivianaAAA <antviviana17@gmail.com>
2022-11-23 10:16:30 +0200

    made the ball jump
    i did the character, fixed some errors, set the onClick and made it jump

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 var block = document.getElementById('block')
 var hole = document.getElementById('hole')
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
-	var random = -((math.random()*300)+150);
+	var random = -((Math.random()*300)+150);
 	hole.style.top = random + "px";
 });
+setInterval (function() {
+	var characterTop = 
+	parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	if (jumping==0) {
+	character.style.top = (characterTop+3)+"px";
+	}
+},10);
+
+function jump() {
+	jumping = 1;
+	let jumpCount = 0;
+	var jumpInterval = setInterval(function(){
+	var characterTop = 
+	parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	if((characterTop>6)&&(jumpCount<15)){
+	character.style.top = (characterTop-5)+"px";
+	}
+	if(jumpCount>20){
+		clearInterval(jumpInterval);
+		jumping=0;
+		jumpCount=0;
+	}
+		
+		jumpCount++;
+	
+	
+	},10)
+}
