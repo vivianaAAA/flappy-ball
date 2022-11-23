a24bae96dd7902fea89cb3aab5d32568196f7338
vivianaAAA <antviviana17@gmail.com>
2022-11-23 10:51:17 +0200

    donee
    i want to do some more styling and css but im happy with the result rn

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 var block = document.getElementById('block')
 var hole = document.getElementById('hole')
 var jumping = 0;
+var counter = 0;
 
 hole.addEventListener('animationiteration', () => {
 	var random = -((Math.random()*300)+150);
 	hole.style.top = random + "px";
+	counter++;
 });
 setInterval (function() {
 	var characterTop = 
 	parseInt(window.getComputedStyle(character).getPropertyValue("top"));
 	if (jumping==0) {
 	character.style.top = (characterTop+3)+"px";
+	}
+	var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+	var cTop = -(500-characterTop);
+	if ((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+		alert ("GAME OVER Score: "+counter);
+		character.style.top = 100 + "px";
+		counter=0
 	}
 },10);
 
